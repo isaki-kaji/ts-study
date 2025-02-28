e525cafa4c3d2f2203b0e879d45978bc7bdc0cc3
masaki_okajima <o.k.j.w.s.i.0123@gmail.com>
2025-02-28 00:28:18 +0000

    コメントを追加

--- pro-ts/chapter01/ex06.ts
+++ pro-ts/chapter01/ex06.ts
@@ -5,6 +5,7 @@ const rl = createInterface({
   output: process.stdout,
 });
 
+// nullかundefinedを判定したい場合は「??」を使う
 rl.question('名前を入力してください.', (name) => {
   const displayName = name || '名無し';
   console.log(`こんにちは、${displayName}さん。`);
