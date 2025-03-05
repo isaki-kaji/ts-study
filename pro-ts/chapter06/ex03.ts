832618bee485def54308d881be15a193912008c8
masaki_okajima <o.k.j.w.s.i.0123@gmail.com>
2025-03-05 15:17:47 +0000

    typeof演算子

--- pro-ts/chapter06/ex03.ts
+++ pro-ts/chapter06/ex03.ts
@@ -3,3 +3,21 @@ const signNumber = (type: 'plus' | 'minus') => {
 };
 
 console.log(signNumber('plus'));
+
+type SignType = 'plus' | 'minus';
+
+const numberWithSign = (num: number, type: SignType | 'none') => {
+  if (type === 'none') {
+    return 0;
+  }
+
+  return num * signNumber(type);
+};
+
+const formatNumberOrString = (value: string | number) => {
+  if (typeof value === 'number') {
+    return value.toFixed(3);
+  } else {
+    return value;
+  }
+};
