0ef14445319812116e5aa1600b5af5911cad4857
masaki_okajima <o.k.j.w.s.i.0123@gmail.com>
2025-03-07 00:36:55 +0000

    tc: hello world

---
+++ type-challenges/warm-up/hello-world.ts
@@ -0,0 +1,7 @@
+import { Equal, Expect } from '@type-challenges/utils';
+
+// expected to be string
+type HelloWorld = string;
+
+// you should make this work
+type test = Expect<Equal<HelloWorld, string>>;
