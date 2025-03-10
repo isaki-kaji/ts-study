ed9eb9edbefde12a36bdc2129d8a9a51110133b1
masaki_okajima <o.k.j.w.s.i.0123@gmail.com>
2025-03-10 14:51:22 +0000

    tc: awaited

---
+++ type-challenges/easy/awaited.ts
@@ -0,0 +1,13 @@
+type Thenable<T> = {
+  then: (onfulfilled: (arg: T) => unknown) => unknown;
+};
+
+type MyAwaited<T extends Thenable<any> | Promise<any>> = T extends Promise<
+  infer Inner
+>
+  ? Inner extends Promise<any>
+    ? MyAwaited<Inner>
+    : Inner
+  : T extends Thenable<infer U>
+  ? U
+  : false;
