5e6fd546f3c6a1aecdf8e35b74d81d679b365d1b
masaki_okajima <o.k.j.w.s.i.0123@gmail.com>
2025-03-11 05:56:35 +0000

    frog

---
+++ algo-data/chapter5/frog.ts
@@ -0,0 +1,24 @@
+export {};
+
+function frog(input: string) {
+  const [[n], h]: number[][] = input
+    .trim()
+    .split('\n')
+    .map((a) => a.split(' ').map((s) => parseInt(s, 10)));
+
+  const dp: number[] = [];
+
+  dp[0] = 0;
+  dp[1] = Math.abs(h[1] - h[0]);
+
+  for (let i = 2; i < n; i++) {
+    dp[i] = Math.min(
+      dp[i - 1] + Math.abs(h[i] - h[i - 1]),
+      dp[i - 2] + Math.abs(h[i] - h[i - 2])
+    );
+  }
+
+  console.log(dp[n - 1]);
+}
+
+frog(require('fs').readFileSync('/dev/stdin', 'utf8'));
