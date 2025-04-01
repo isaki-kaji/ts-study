d925ff4700fe07f982d8b15139f415bdea00ecb3
masaki_okajima <o.k.j.w.s.i.0123@gmail.com>
2025-04-01 07:21:49 +0000

    rf: ch06 関数の抽出 after

--- refactoring/ch06/extractFunc.ts
+++ refactoring/ch06/extractFunc.ts
@@ -1,24 +1,37 @@
-function printOwing(invoice) {
-  let outstanding = 0;
+type Invoice = {
+  orders: { amount: number }[];
+  dueDate: Date;
+  customer: string;
+};
 
-  console.log('***********************');
-  console.log('**** Customer Owes ****');
-  console.log('***********************');
+function printOwing(invoice: Invoice) {
+  printBanner();
+  recordDueDate(invoice);
+  printDetails(invoice, calcOutstanding(invoice));
+}
 
-  // 未払い金の計算（calculate outstanding）
-  for (const o of invoice.orders) {
-    outstanding += o.amount;
-  }
+function calcOutstanding(invoice: Invoice): number {
+  return invoice.orders
+    .map((order) => order.amount)
+    .reduce((acc, amount) => acc + amount, 0);
+}
 
-  // 締め日の記録（record due date）
+function recordDueDate(invoice: Invoice): void {
   const today = Clock.today;
   invoice.dueDate = new Date(
     today.getFullYear(),
     today.getMonth(),
     today.getDate() + 30
   );
+}
 
-  // 明細の印字（print details）
+function printBanner(): void {
+  console.log('***********************');
+  console.log('**** Customer Owes ****');
+  console.log('***********************');
+}
+
+function printDetails(invoice: Invoice, outstanding: number): void {
   console.log(`name: ${invoice.customer}`);
   console.log(`amount: ${outstanding}`);
   console.log(`due: ${invoice.dueDate.toLocaleDateString()}`);
