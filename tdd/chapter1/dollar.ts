a0f2a6a44feb1abc4727c9ad3496e28c9c87b7fe
masaki_okajima <o.k.j.w.s.i.0123@gmail.com>
2025-04-14 23:01:31 +0000

    tdd: equalsを実装

--- tdd/chapter1/dollar.ts
+++ tdd/chapter1/dollar.ts
@@ -8,4 +8,8 @@ export class Dollar {
   public times(multiplier: number): Dollar {
     return new Dollar(this.amount * multiplier);
   }
+
+  public equals(dollar: Dollar): boolean {
+    return this.amount === dollar.amount;
+  }
 }
--- tdd/chapter1/money.test.ts
+++ tdd/chapter1/money.test.ts
@@ -1,10 +1,17 @@
 import { Dollar } from './dollar';
 
-test('multiplication', () => {
-  const five = new Dollar(5);
-  let product: Dollar = five.times(2);
-  expect(product.amount).toBe(10);
+describe('Dollar Test', () => {
+  test('multiplication', () => {
+    const five = new Dollar(5);
+    let product: Dollar = five.times(2);
+    expect(product.amount).toBe(10);
 
-  product = five.times(3);
-  expect(product.amount).toBe(15);
+    product = five.times(3);
+    expect(product.amount).toBe(15);
+  });
+
+  test('equal', () => {
+    expect(new Dollar(5).equals(new Dollar(5))).toBe(true);
+    expect(new Dollar(5).equals(new Dollar(6))).toBe(false);
+  });
 });
