febf73d7762f9a160b3bd3bb77827fb7addc052b
masaki_okajima <o.k.j.w.s.i.0123@gmail.com>
2025-04-14 03:03:58 +0000

    tdd: 副作用をなくす

--- tdd/chapter1/dollar.ts
+++ tdd/chapter1/dollar.ts
@@ -1,11 +1,11 @@
 export class Dollar {
-  constructor(private _amount: number) {}
+  constructor(private readonly _amount: number) {}
 
   get amount(): number {
     return this._amount;
   }
 
-  public times(multiplier: number): void {
-    this._amount *= multiplier;
+  public times(multiplier: number): Dollar {
+    return new Dollar(this.amount * multiplier);
   }
 }
--- tdd/chapter1/money.test.ts
+++ tdd/chapter1/money.test.ts
@@ -2,6 +2,9 @@ import { Dollar } from './dollar';
 
 test('multiplication', () => {
   const five = new Dollar(5);
-  five.times(2);
-  expect(five.amount).toBe(10);
+  let product: Dollar = five.times(2);
+  expect(product.amount).toBe(10);
+
+  product = five.times(3);
+  expect(product.amount).toBe(15);
 });
